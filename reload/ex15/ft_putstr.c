783966ea51c27fb2ee9716a35ed9d75c11d1d8a3
Maksim Georgiev Marinov <mmarinov@cbr9s6.42barcelona.com>
2024-06-21 10:39:15 +0200

    primer commit

---
+++ reload/ex15/ft_putstr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmarinov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 12:51:18 by mmarinov          #+#    #+#             */
+/*   Updated: 2024/06/10 10:48:54 by mmarinov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+/*
+int main(void)
+{
+char    *message = "Hello World!";
+ft_putstr(message);
+return (0);
+    }
+    */
