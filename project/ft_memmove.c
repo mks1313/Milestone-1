f67e3a04476ef2eab8fc227075015471e9a0acdb
Maksim Georgiev Marinov <mmarinov@cbr9s2.42barcelona.com>
2024-06-27 10:33:30 +0200

    functions

--- project/ft_memmove.c
+++ project/ft_memmove.c
@@ -6,13 +6,11 @@
 /*   By: mmarinov <mmarinov@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 15:49:00 by mmarinov          #+#    #+#             */
-/*   Updated: 2024/06/24 16:32:55 by mmarinov         ###   ########.fr       */
+/*   Updated: 2024/06/26 11:41:45 by mmarinov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <string.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
@@ -24,7 +22,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	source = (unsigned char *)src;
 	if (!dest || !src)
 		return (NULL);
-	if (source < destiny && source + n > destiny)
+	if (source < destiny)
 	{
 		count = n;
 		while (count > 0)
@@ -35,58 +33,23 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	}
 	else
 	{
-		count = 0;
-		while (count < n)
-			destiny[count] = source[count];
-		count++;
+		ft_memcpy(dest, src, n);
 	}
 	return (dest);
 }
+/*
+#include <string.h>
+#include <stdio.h>
 
 int	main(void)
 {
-// Caso 1: Copia simple sin solapamiento
-	char	src1[] = "Hello, World!";
-	char	dest1[20];
-	char	dest1_std[20];
+	unsigned char	source[] = "Hello, World!";
+	unsigned char	destiny1[20] = {0};
+	unsigned char	destiny2[20] = {0};
 
-	ft_memmove(dest1, src1, ft_strlen(src1) + 1);
-	memmove(dest1_std, src1, ft_strlen(src1) + 1);
-	printf("Caso 1 - Sin solapamiento:\n");
-	printf("ft_memmove: %s\n", dest1);
-	printf("memmove: %s\n", dest1_std);
-	// Caso 2: Copia con solapamiento, src antes de dest
-	char	src2[] = "Overlap Example";
-	char	*dest2 = src2 + 8;
-	ft_memmove(dest2, src2, 8);
-	memmove(dest2, src2, 8);
-
-	printf("Caso 2 - Con solapamiento (src antes de dest):\n");
-	printf("ft_memmove: %s\n", dest2);
-	printf("memmove: %s\n", dest2);
-
-	// Caso 3: Copia con solapamiento, dest antes de src
-	char	src3[] = "Another Example";
-	char	*dest3 = src3;
-	char	*src3_part = src3 + 8;
-
-	ft_memmove(dest3, src3_part, 8);
-	memmove(dest3, src3_part, 8);
-	printf("Caso 3 - Con solapamiento (dest antes de src):\n");
-	printf("ft_memmove: %s\n", dest3);
-	printf("memmove: %s\n", dest3);
-	// Comprobación visual de igualdad
-	if (strcmp(dest1, dest1_std) == 0)
-		printf("Caso 1 - Las cadenas son iguales.\n");
-	else
-		printf("Caso 1 - Las cadenas NO son iguales.\n");
-	if (strcmp(src2 + 8, dest2) == 0)
-		printf("Caso 2 - Las cadenas son iguales.\n");
-	else
-		printf("Caso 2 - Las cadenas NO son iguales.\n");
-	if (strcmp(dest3, src3) == 0)
-		printf("Caso 3 - Las cadenas son iguales.\n");
-	else
-		printf("Caso 3 - Las cadenas NO son iguales.\n");
-	return 0;
-}
+	ft_memmove(destiny1, source, strlen((char *)source) + 1);
+	printf("esto es mi funcion: %s \n", destiny1);
+	memmove(destiny2, source, strlen((char *)source) + 1);
+	printf("esto es ofical memmove: %s \n", destiny2);
+	return (0);
+}*/
