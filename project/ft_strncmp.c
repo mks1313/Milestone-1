81aa5aca530ece6795939980c33a09655261aa79
Maksim Georgiev Marinov <mmarinov@cbr9s2.42barcelona.com>
2024-06-28 13:01:01 +0200

    functions

--- project/ft_strncmp.c
+++ project/ft_strncmp.c
@@ -1,4 +1,4 @@
-
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
@@ -6,7 +6,7 @@
 /*   By: mmarinov <mmarinov@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 14:59:52 by mmarinov          #+#    #+#             */
-/*   Updated: 2024/06/26 17:10:00 by mmarinov         ###   ########.fr       */
+/*   Updated: 2024/06/27 15:22:11 by mmarinov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,25 +20,38 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (diff < n && (s1[diff] != '\0' && s2[diff] != '\0'))
 	{
 		if (s1[diff] != s2[diff])
-				{
-				return ((unsigned char)s1[diff] - (unsigned char)s2[diff]);
-				}
-		diff++;
-		if (diff == n)
 		{
-		return ((unsigned char)s1[diff] -(unsigned char)s2[diff]);
+			return ((unsigned char)s1[diff] - (unsigned char)s2[diff]);
 		}
-		return(0);
+		diff++;
 	}
-
+	if (diff == n)
+	{
+		return (0);
+	}
+	return ((unsigned char)s1[diff] -(unsigned char)s2[diff]);
+}
+/*
 #include <stdio.h>
 
 int	main(void)
 {
-char	*temp1 = "manIcomio";
-char	*temp2 = "manafomnio";
-size_t	num = 5;
-int	result = ft_strncmp(temp1, temp2, num);
-printf("la diferencia es: %d", result);
-return(0);
-}
+	char	temp1[] = "manIcomio";
+	char	temp2[] = "manafomnio";
+	size_t	num = 5;
+	int	result = ft_strncmp(temp1, temp2, num);
+	if (result < 0)
+	{
+		printf("temp1 es menor que temp2\n");
+	}
+	else if (result > 0)
+	{
+		printf("temp1 es mayor que temp2\n");
+	}
+	else 
+	{
+		printf("temp1 y temp2 son iguales hasta  %zu caracteres\n", num);
+	}
+	printf("Resultado de la funcion FT_STRNCMP es %d", result);
+	return(0);
+}*/
