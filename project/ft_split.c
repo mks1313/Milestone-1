4f9404a90ebcde6152311e266adde59e04df954f
Maksim Georgiev Marinov <mmarinov@car11s1.42barcelona.com>
2024-07-10 15:50:31 +0200

    some more functions

--- project/ft_split.c
+++ project/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mmarinov <mmarinov@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 16:06:03 by mmarinov          #+#    #+#             */
-/*   Updated: 2024/07/09 18:58:30 by mmarinov         ###   ########.fr       */
+/*   Updated: 2024/07/10 15:27:37 by mmarinov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,23 +28,20 @@ static char	count_word(char const *s, char c)
 			count++;
 		}
 		else if (*s == c)
-		{
 			in_word = 0;
-		}
 		s++;
 	}
 	return (count);
 }
 
-static char *ft_strndup(const chgar *s, size_t n)
+static char	*ft_strndup(const char *s, size_t n)
 {
 	char	*copy;
 	size_t	i;
 
 	copy = (char *)malloc((n + 1) * sizeof(char));
-	if(!copy)
+	if (!copy)
 		return (NULL);
-
 	i = 0;
 	while (i < n)
 	{
@@ -55,7 +52,7 @@ static char *ft_strndup(const chgar *s, size_t n)
 	return (copy);
 }
 
-static void	free_array(char const **array, size_t count)
+static void	free_array(char **array, size_t count)
 {
 	size_t	i;
 
@@ -64,40 +61,57 @@ static void	free_array(char const **array, size_t count)
 		free(array[i]);
 		i++;
 	}
-	return (array);
-}
-
-static void	ft_length()
-{
-
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**new_array;
-	size_t count;
+	size_t	count;
+	size_t	len;
 
+	if (!s)
+		return (NULL);
+	count = 0;
 	new_array = (char **)ft_calloc(count_word(s, c) + 1, sizeof(char *));
 	if (!new_array)
 		return (NULL);
-	count = 0;
 	while (*s)
 	{
 		if (*s != c)
 		{
-			len = 
-			new_array[count] = ft_substr(s, 0, len);
+			len = 0;
+			while (s[len] && s[len] != c)
+				len++;
+			new_array[count] = ft_strdup(s);
 			if (!new_array[count])
 			{
-				
+				free_array(new_array, count);
 				return (NULL);
 			}
 			count++;
-			s = s + len;
+			s += len;
 		}
 		while (*s == c)
 			s++;
-
+		new_array[count] = NULL;
 	}
+	return (new_array);
 }
+/*
+#include <stdio.h>
 
+	int main(void)
+	{
+		char **result;
+		size_t i;
+
+		result = ft_split("Hola mundo, bienvenidos al 42!", ' ');
+		if (result) {
+			for (i = 0; result[i] != NULL; i++) {
+				printf("%s\n", result[i]);
+				free(result[i]); // Liberar cada subcadena individualmente
+			}
+			free(result); // Liberar el arreglo de punteros
+		}
+		return 0;
+	}*/
