c7385349d37892c0a67d30a3429db371df299602
Maksim Georgiev Marinov <mmarinov@car7s4.42barcelona.com>
2024-07-25 16:04:45 +0200

    libft

---
+++ ft_printf/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int	main()
+{
+	char ch = 'c';
+	char * s = "Max";
+
+	printf("character %c\n %s\n %p\n", ch, s, s);
+}
--- ft_printf/utils2.c
+++ ft_printf/utils2.c
@@ -1,46 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   convert.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmarinov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/20 13:13:11 by mmarinov          #+#    #+#             */
-/*   Updated: 2024/07/24 12:45:14 by mmarinov         ###   ########.fr       */
+/*   Created: 2024/07/20 13:14:57 by mmarinov          #+#    #+#             */
+/*   Updated: 2024/07/20 13:41:31 by mmarinov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
-#include <stdarg.h>
-#include <stdio.h>
-
-int	ft_printf(char const *s, ...)
-{
-	int	count;
-	va_list	string;
-
-	count = 0;
-	va_start(string, s);
-	if (*s == '%')
-	{
-		s++;
-	}
-	if (*s == 'c')
-	{
-		ft_putchar_fd(va_arg(string, int), 1);
-		s++;
-		count++;
-	}
-	va_end(string);
-	return (count);
-}
-
-int	main(void)
-{
-	char	c;
-
-	c = 'f';
-	printf("original: %c\n", c);
-	ft_printf("%c", c);
-	return(0);
-}
